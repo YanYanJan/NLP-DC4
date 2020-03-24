f480769d26d10b02e847a39335741d847349a0c3
Unknown <jenny9993@hotmail.com>
2020-03-23 22:15:08 -0700

    Finished Code

--- DC4.py
+++ DC4.py
@@ -1,8 +1,132 @@
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
+import re,os
+from string import punctuation
+from nltk.stem import SnowballStemmer
+from nltk.corpus import stopwords
 
-data =  pd.read_csv('/Users/yanyan/PycharmProjects/DC4/quora_duplicate_questions.tsv', sep='\t',error_bad_lines=False)
-data = data.drop(['id','qid1','qid2'], axis = 1)
+path = os.getcwd()+"/quora_duplicate_questions.tsv"
+df =  pd.read_csv(path, sep='\t',error_bad_lines=False)
+df = df.drop(['id','qid1','qid2'], axis = 1)
 
 #print(data)
+
+SPECIAL_TOKENS = {
+    'quoted': 'quoted_item',
+    'non-ascii': 'non_ascii_word',
+    'undefined': 'something'
+}
+
+def clean(text, stem_words=True):
+
+    def pad_str(s):
+        return ' '+s+' '
+
+    if pd.isnull(text):
+        return ''
+
+    stops = set(stopwords.words("english"))
+
+    # Empty question
+
+    if type(text) != str or text=='':
+        return ''
+
+    #The rules below are found in https://www.kaggle.com/currie32/the-importance-of-cleaning-text
+    # Clean the text
+    text = re.sub("\'s", " ", text) # we have cases like "Sam is" or "Sam's" (i.e. his) these two cases aren't separable, I choose to compromise are kill "'s" directly
+    text = re.sub(" whats ", " what is ", text, flags=re.IGNORECASE)
+    text = re.sub("\'ve", " have ", text)
+    text = re.sub("can't", "can not", text)
+    text = re.sub("n't", " not ", text)
+    text = re.sub("i'm", "i am", text, flags=re.IGNORECASE)
+    text = re.sub("\'re", " are ", text)
+    text = re.sub("\'d", " would ", text)
+    text = re.sub("\'ll", " will ", text)
+    text = re.sub("e\.g\.", " eg ", text, flags=re.IGNORECASE)
+    text = re.sub("b\.g\.", " bg ", text, flags=re.IGNORECASE)
+    text = re.sub("(\d+)(kK)", " \g<1>000 ", text)
+    text = re.sub("e-mail", " email ", text, flags=re.IGNORECASE)
+    text = re.sub("(the[\s]+|The[\s]+)?U\.S\.A\.", " America ", text, flags=re.IGNORECASE)
+    text = re.sub("(the[\s]+|The[\s]+)?United State(s)?", " America ", text, flags=re.IGNORECASE)
+    text = re.sub("\(s\)", " ", text, flags=re.IGNORECASE)
+    text = re.sub("[c-fC-F]\:\/", " disk ", text)
+
+    # remove comma between numbers
+    text = re.sub('(?<=[0-9])\,(?=[0-9])', "", text)
+    text = re.sub('\$', " dollar ", text)
+    text = re.sub('\%', " percent ", text)
+    text = re.sub('\&', " and ", text)
+    text = re.sub('[^\x00-\x7F]+', pad_str(SPECIAL_TOKENS['non-ascii']), text) # replace non-ascii word with special word
+
+
+    text = re.sub("(?<=[0-9])rs ", " rs ", text, flags=re.IGNORECASE)
+    text = re.sub(" rs(?=[0-9])", " rs ", text, flags=re.IGNORECASE)
+    text = re.sub(r" (the[\s]+|The[\s]+)?US(A)? ", " America ", text)
+    text = re.sub(r" UK ", " England ", text, flags=re.IGNORECASE)
+    text = re.sub(r" imrovement ", " improvement ", text, flags=re.IGNORECASE)
+    text = re.sub(r" intially ", " initially ", text, flags=re.IGNORECASE)
+    text = re.sub(r" quora ", " Quora ", text, flags=re.IGNORECASE)
+    text = re.sub(r" dms ", " direct messages ", text, flags=re.IGNORECASE)
+    text = re.sub(r" demonitization ", " demonetization ", text, flags=re.IGNORECASE)
+    text = re.sub(r" actived ", " active ", text, flags=re.IGNORECASE)
+    text = re.sub(r" kms ", " kilometers ", text, flags=re.IGNORECASE)
+    text = re.sub(r" cs ", " computer science ", text, flags=re.IGNORECASE)
+    text = re.sub(r" upvote", " up vote", text, flags=re.IGNORECASE)
+    text = re.sub(r" iPhone ", " phone ", text, flags=re.IGNORECASE)
+    text = re.sub(r" \0rs ", " rs ", text, flags=re.IGNORECASE)
+    text = re.sub(r" calender ", " calendar ", text, flags=re.IGNORECASE)
+    text = re.sub(r" ios ", " operating system ", text, flags=re.IGNORECASE)
+    text = re.sub(r" gps ", " GPS ", text, flags=re.IGNORECASE)
+    text = re.sub(r" gst ", " GST ", text, flags=re.IGNORECASE)
+    text = re.sub(r" programing ", " programming ", text, flags=re.IGNORECASE)
+    text = re.sub(r" bestfriend ", " best friend ", text, flags=re.IGNORECASE)
+    text = re.sub(r" dna ", " DNA ", text, flags=re.IGNORECASE)
+    text = re.sub(r" III ", " 3 ", text)
+    text = re.sub(r" banglore ", " Banglore ", text, flags=re.IGNORECASE)
+    text = re.sub(r" J K ", " JK ", text, flags=re.IGNORECASE)
+    text = re.sub(r" J\.K\. ", " JK ", text, flags=re.IGNORECASE)
+
+    # replace the float numbers with a random number
+    text = re.sub('[0-9]+\.[0-9]+', " 87 ", text)
+
+    # Remove punctuation from text
+    text = ''.join([c for c in text if c not in punctuation]).lower()
+       # Return a list of words
+    return text
+
+df['question1'] = df['question1'].apply(clean)
+df['question2'] = df['question2'].apply(clean)
+
+
+# Vectorize the data
+
+count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+count_vect.fit(pd.concat((df['question1'],df['question2'])).unique())
+trainq1_trans = count_vect.transform(df['question1'].values)
+trainq2_trans = count_vect.transform(df['question2'].values)
+labels = df['is_duplicate'].values
+
+import scipy
+from scipy.sparse import coo_matrix, hstack
+
+# Split the data in to training and testing
+X = scipy.sparse.hstack((trainq1_trans,trainq2_trans))
+y = labels
+X_train,X_test,y_train,y_test = train_test_split(X,y, test_size = 0.2, random_state = 1)
+
+
+## Thanks to xgboost it takes care of a lot of things
+classifer = xgb.XGBClassifier(max_depth=50, n_estimators=80, learning_rate=0.1, colsample_bytree=.7, gamma=0, reg_alpha=4, objective='binary:logistic', eta=0.3, silent=1, subsample=0.8).fit(X_train, y_train)
+prediction = classifer.predict(X_test)
+
+
+#Test for the performance
+from sklearn.metrics import f1_score, classification_report, accuracy_score, confusion_matrix
+
+print(classification_report(y_test ,prediction ))
+print('Confusion Matrix: \n',confusion_matrix(y_test,prediction))
+print('Accuracy: ', accuracy_score(y_test,prediction))
